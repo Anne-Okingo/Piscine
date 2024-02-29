9ff390805d9986a880739818c26ffa9f3c1ac2f9
aokingo <annekadiso@gmail.com>
2024-02-29 21:24:40 +0300

    printdigits

--- printdigits/main.go
+++ printdigits/main.go
@@ -2,8 +2,8 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main (){
-	for i :='0'; i <= '9'; i++ {
+func main () {
+	for i := '0'; i <= '9'; i++ {
 		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')

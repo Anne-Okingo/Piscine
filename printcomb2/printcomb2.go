be9c5208e18d344c2f7d4373717ba3aa916e03ea
aokingo <annekadiso@gmail.com>
2024-03-07 21:33:01 +0300

    printcomb2.go

--- printcomb2/printcomb2.go
+++ printcomb2/printcomb2.go
@@ -3,13 +3,28 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for a := 00; a <= 98; a++ {
-		b := 00 + 1; b <= 99; b++ {
-			if a != 98 && a != 99 {
-			z01.PrintRune(a)
-			z01.PrintRune(b)
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				for d := '0'; d <= '9'; d++ {
+					if a == '9' && b == '8' && c == '9' && d == '9' {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(' ')
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+					} else if a == c || b < d || a < d {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(' ')
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
 			}
-			z01.PrintRune('\n')
 		}
 	}
+	z01.PrintRune('\n')
 }

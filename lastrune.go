186348362fa08e25c20f338752973fe82793b6cb
Christopher Fremond <dev@01-edu.org>
2024-03-08 12:57:58 +0000

    Update 'lastrune.go'

--- lastrune.go
+++ lastrune.go
@@ -1,15 +1,5 @@
 package piscine
-
 func LastRune(s string) rune {
-      runes := []rune(s)
-      lens := 0
-      for range runes {
-        lens++
-      }
-  for index,value := range{
-    if index == lens-1 {
-      return value
-    }
-  }
-  return 0
+  last := []rune(s)
+  return last[len(s)-1]
 }

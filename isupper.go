f84f96232aa5e75021614eeb73037148e6001c48
aokingo <annekadiso@gmail.com>
2024-03-08 19:56:29 +0300

    isupper.go

---
+++ isupper.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsUpper(s string) bool {
+	for _, r := range s {
+		if r >= 'A' && r <= 'Z' {
+			continue
+		} else if r >= 'a' && r < +'z' {
+			return false
+		} else {
+			return false
+		}
+	}
+	return true
+}

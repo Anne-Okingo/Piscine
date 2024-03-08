d7f086ec5f7058201eb3bc52a5425b41157caed8
aokingo <annekadiso@gmail.com>
2024-03-08 20:19:15 +0300

    isaplha.go

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string)bool {
 	for _, i := range s {
-		is(i>='a' && i <='z') || (i >= 'A' && i <= 'z') || (i >= '0' && i <= '9') {
+		if (i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') || (i >= '0' && i <= '9') {
 			continue
 		}else {
 			return false

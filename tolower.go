fcbc369b52dfd377f47f16e459868dbfa57b7d05
aokingo <annekadiso@gmail.com>
2024-03-08 21:06:17 +0300

    tolower.go

--- tolower.go
+++ tolower.go
@@ -2,12 +2,12 @@ package piscine
 
 func ToLower(s string) string {
 	var result string
-	for _, char := range s{
+	for _, char := range s {
 		if char >= 'A' && char <= 'z' {
 			result += string(char + 32)
 		} else {
 			result += string(char)
-		} 
+		}
 	}
 	return result
 }

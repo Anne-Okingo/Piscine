9d548a25d151842cfea0f1b354b85825d1151c96
aokingo <annekadiso@gmail.com>
2024-03-08 20:13:57 +0300

    islower.go

--- islower.go
+++ islower.go
@@ -3,10 +3,12 @@ package piscine
 func IsLower(s string) bool {
 	for _, r := range s {
 		if r >= 'a' && r <= 'z' {
+			continue
+		} else if r > +'A' && r <= 'Z' {
 			return false
 		} else {
 			return false
 		}
 	}
-	return false
+	return true
 }

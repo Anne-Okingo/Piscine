82af52fddcd59e926237f6e45036a75fbca92b64
aokingo <annekadiso@gmail.com>
2024-03-08 20:29:37 +0300

    isaplha.go

--- isalpha.go
+++ isalpha.go
@@ -4,7 +4,7 @@ func IsAlpha(s string)bool {
 	for _, i := range s {
 		if (i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') || (i >= '0' && i <= '9') {
 			continue
-		}else {
+		} else {
 			return false
 		}
 	}

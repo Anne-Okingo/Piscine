8d4d11963d536ab52d7ebf10f09c1df4e677f9fc
aokingo <annekadiso@gmail.com>
2024-03-10 17:38:15 +0300

    concatparams.go

--- concatparams.go
+++ concatparams.go
@@ -1,9 +1,9 @@
 package piscine
 
 func ConcatParams(arg []string) string {
-	result := arg [0]
-	for_, s := range args [1:] {
-		result += "\n" + s
+	result := arg[0]
+	for _, s := range arg[1:] {
+		result = result + "\n" + s
 	}
 	return result
 }

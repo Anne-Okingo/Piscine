9438baa6008f511a068dc826b4859974f06551d4
aokingo <annekadiso@gmail.com>
2024-03-12 18:46:32 +0300

    strrev.go

---
+++ strrev.go
@@ -0,0 +1,11 @@
+package piscine
+
+func StrRev(s string) string {
+	p := []rune(s)
+	var t []rune
+	length := len(p)
+	for i := length - 1; i >= 0; i-- {
+		t = append(t, p[i])
+	}
+	return string(t)
+}

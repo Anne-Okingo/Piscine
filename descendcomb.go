f02aa8974c653e04b0e0ac0b59c1291495d75662
aokingo <annekadiso@gmail.com>
2024-03-13 14:41:50 +0300

    collatzcountdown.go

--- descendcomb.go
+++ descendcomb.go
@@ -6,14 +6,14 @@ func DescendComb() {
 	for a := '9'; a >= '0'; a-- {
 		for b := '9'; b >= '1'; b-- {
 			for c := '9'; c >= '0'; c-- {
-				for d := '8'; d >= '0'; d-- {
+				for d := '9'; d >= '0'; d-- {
 					if a == '0' && b == '1' && c == '0' && d == '0' {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
 						z01.PrintRune(c)
 						z01.PrintRune(d)
-					} else if a == c || d > b || d > a {
+					} else if a == c || b > d || a > d {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')

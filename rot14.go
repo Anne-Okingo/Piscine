a3ed0680defe85323c9f2e766e86a5faa72af694
Christopher Fremond <dev@01-edu.org>
2024-03-13 08:08:32 +0000

    Update 'rot14.go'

--- rot14.go
+++ rot14.go
@@ -1,11 +1,11 @@
 package piscine
 
 func Rot14(s string) string {
-  result := ""
+  var result string
   
   for _, ch := range s {
     if ch >= 'a' && ch <= 'z' {
-      result = result + string('a' + (ch-'z'+14)%26)
+      result = result + string('a' + (ch-'a'+14)%26)
     }else if ch >= 'A' && ch >= 'Z' {
       result = result + string('A' + (ch-'Z'+14)%26)
     }else {

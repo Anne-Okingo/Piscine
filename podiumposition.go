2b66e278e551648ed3fd88f0d3e5b4ae416df152
aokingo <annekadiso@gmail.com>
2024-03-14 04:00:55 +0300

    podiumposition.go

--- podiumposition.go
+++ podiumposition.go
@@ -1,8 +1,8 @@
 package piscine
 
 func PodiumPosition(podium [][]string) [][]string {
-for i, j := 0, len(podium)-1; 1 < j; i, j = 1+1, j-1 {
-	podium[i], podium[j] = podium[j], podium[i]
-}
-return podium
+	for i, j := 0, len(podium)-1; 1 < j; i, j = 1+1, j-1 {
+		podium[i], podium[j] = podium[j], podium[i]
+	}
+	return podium
 }

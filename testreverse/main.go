dbb9b533dc76ec1d47223ad073ea2fa229ce75f1
aokingo <annekadiso@gmail.com>
2024-03-14 06:45:06 +0300

    files

--- testreverse/main.go
+++ testreverse/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-        "fmt"
-        "piscine"
+	"fmt"
+	"piscine"
 )
 
 func main() {
-        fmt.Println(piscine.ReverseMenuIndex([]string{"desserts", "mains", "drinks", "starters"}))
+	fmt.Println(piscine.ReverseMenuIndex([]string{"desserts", "mains", "drinks", "starters"}))
 }

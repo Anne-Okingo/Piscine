53e4d61ccd71f0122999cdd9cd33c9c44931d6b2
aokingo <annekadiso@gmail.com>
2024-03-14 09:56:00 +0300

    displayfile

--- displayfile/main.go
+++ displayfile/main.go
@@ -8,12 +8,21 @@ import (
 
 func main() {
 	args := os.Args[1:]
+
 	if len(args) == 0 {
 		fmt.Println("File name missing")
-	} else if len(args) != 1 {
-		fmt.Println("Too many arguments")
-	} else {
-		filecontent, _ := ioutil.ReadFile(args[10])
-		fmt.Printf("%s", filecontent)
+		return
 	}
+
+	if len(args) > 1 {
+		fmt.Println("Too many arguments")
+		return
+	}
+
+	content, err := ioutil.ReadFile(args[0])
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+	fmt.Print(string(content))
 }

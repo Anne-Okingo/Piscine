778dbcc26792c7a5028f5d9e4fd56cc56fd0222b
aokingo <annekadiso@gmail.com>
2024-03-16 20:13:49 +0300

    printcomb2.go

--- printcomb2/printcomb2.go
+++ printcomb2/printcomb2.go
@@ -7,14 +7,12 @@ func PrintComb2() {
 		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
 				for d := '0'; d <= '9'; d++ {
-
 					if a < c || (a == c && b < d) {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
 						z01.PrintRune(c)
 						z01.PrintRune(d)
-
 						if !(a == '9' && b == '8' && c == '9' && d == '9') {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
@@ -26,5 +24,4 @@ func PrintComb2() {
 		}
 	}
 	z01.PrintRune('\n')
-
 }

0c83f8ecf84c993825e973d98c7ac76c1fcdad26
aokingo <annekadiso@gmail.com>
2024-03-18 18:55:58 +0300

    printparams

--- printparams/main.go
+++ printparams/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"os"
+
 	"github.com/01-edu/z01"
 )
 
 func main() {
 	params := os.Args[1:]
 	for _, arguments := range params {
-		for _,value := range arguments {
+		for _, value := range arguments {
 			z01.PrintRune(value)
 		}
 		z01.PrintRune('\n')

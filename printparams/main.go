9f3b5071ba88339c7f9ef9cdf8e410d160d8a9dc
aokingo <annekadiso@gmail.com>
2024-03-18 18:53:41 +0300

    printparams

---
+++ printparams/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	params := os.Args[1:]
+	for _, arguments := range params {
+		for _,value := range arguments {
+			z01.PrintRune(value)
+		}
+		z01.PrintRune('\n')
+	}
+}

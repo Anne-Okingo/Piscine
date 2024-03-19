9db7abd7d2572696bcbbefcd9982203c0c5beacf
aokingo <annekadiso@gmail.com>
2024-03-19 17:44:48 +0300

    descendcomb.go

--- descendcomb.go
+++ descendcomb.go
@@ -5,27 +5,19 @@ import (
 )
 
 func DescendComb() {
-	for a := '9'; a >= '0'; a-- {
-		for b := '9'; b >= '0'; b-- {
-			for c := '9'; c >= '0'; c-- {
-				for d := '9'; d >= '0'; d-- {
-					if a == c && b == d {
-						continue
-					}
-					if a > c || (a == c && b > d) {
-						z01.PrintRune(rune(a))
-						z01.PrintRune(rune(b))
-						z01.PrintRune(' ')
-						z01.PrintRune(rune(c))
-						z01.PrintRune(rune(d))
-						if a != '0' || b != '1' || c != '0' || d != '0' {
-							z01.PrintRune(',')
-							z01.PrintRune(' ')
-						}
-					}
-				}
+	for r := 99; r >= 1; r-- {
+		for s := r - 1; s >= 0; s-- {
+			z01.PrintRune(rune(r/10) + '0')
+			z01.PrintRune(rune(r%10) + '0')
+			z01.PrintRune(' ')
+			z01.PrintRune(rune(s/10) + '0')
+			z01.PrintRune(rune(s%10) + '0')
+			if r != 1 || s != 0 {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			}
+
 		}
+
 	}
-	z01.PrintRune('\n')
 }

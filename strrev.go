e106f66602e8d42071500cd5da09625155d9320b
aokingo <annekadiso@gmail.com>
2024-03-20 14:56:48 +0300

    strrev.go

--- strrev.go
+++ strrev.go
@@ -3,8 +3,8 @@ package piscine
 func StrRev(s string) string {
 	p := []rune(s)
 	   t := []rune{}
-	      length := len(p)
-	      for i := length - 1; i >= 0; i-- {
+	   length := len(p)
+	   for i := length - 1; i >= 0; i-- {
 		    t = append(t, p[i])
 	}
 	return string(t)

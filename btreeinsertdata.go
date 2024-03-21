fb0aa6597cab25737d07d59bbf0bc435cbd0bcec
aokingo <annekadiso@gmail.com>
2024-03-21 19:11:31 +0300

    btreeinsertdata.go

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -1,20 +1,29 @@
 package piscine
 
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
+// type TreeNode struct {
+// 	Left, Right, Parent *TreeNode
+// 	Data                string
+// }
 
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+// 	if root == nil {
+// 		return &TreeNode{Data: data}
+// 	}
+// 	if data < root.Data {
+// 		root.Left = BTreeInsertData(root.Left, data)
+// 		root.Left.Parent = root
+// 	} else if data > root.Data {
+// 		root.Right = BTreeInsertData(root.Right, data)
+// 		root.Right.Parent = root
+// 	}
+// 	return root
+// }
+
+func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
-		return &TreeNode{Data: data}
+		return
 	}
-	if data < root.Data {
-		root.Left = BTreeInsertData(root.Left, data)
-		root.Left.Parent = root
-	} else if data > root.Data {
-		root.Right = BTreeInsertData(root.Right, data)
-		root.Right.Parent = root
-	}
-	return root
+	BTreeApplyInorder(root.Left, f)
+	f(root.Data)
+	BTreeApplyInorder(root.Right, f)
 }
